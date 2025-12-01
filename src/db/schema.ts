5781959a4d253552b492a52c63202f0367bdf87e
Do Dinh Thy Son <tdo@ethinking.de>
2025-12-01 13:10:53 +0700

    Add drizzle connection

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from "drizzle-kit";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { env } from "@/common/utils/envConfig";
 
 export default defineConfig({
 	dialect: "postgresql", // 'mysql' | 'sqlite' | 'turso'
 	schema: "./src/db/schema.ts",
 });
+
+export const db = drizzle({
+	connection: {
+		connectionString: env.DATABASE_URL,
+		ssl: true,
+	},
+});
+
+export const result = await db.execute("SELECT 1");
